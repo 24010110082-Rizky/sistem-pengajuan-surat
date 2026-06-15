a230690c740f72730ebe067c790ca411f2b5e832
Rizky <xynnaja701@gmail.com>
2026-06-15 21:46:47 +0800

    Menambahkan routing untuk halaman login dan logout

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,3 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['login']  = 'Auth/index';
+$route['logout'] = 'Auth/logout';
