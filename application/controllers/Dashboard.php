ddc9cb2e989c6c34aadcaec00c18bf60250eaf9e
Rizky <xynnaja701@gmail.com>
2026-06-15 22:58:39 +0800

    Menambahkan pengambilan data statistik pengajuan untuk dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -18,6 +18,12 @@ class Dashboard extends CI_Controller {
     {
         if ($this->session->userdata('role_name') === 'admin') {
             $this->load->model(['Pengajuan_model']);
+            $data['title']          = 'Dashboard Admin';
+            $data['total']          = $this->Pengajuan_model->count_all();
+            $data['menunggu']       = $this->Pengajuan_model->count_by_status('menunggu');
+            $data['diproses']       = $this->Pengajuan_model->count_by_status('diproses');
+            $data['selesai']        = $this->Pengajuan_model->count_by_status('selesai');
+            $data['ditolak']        = $this->Pengajuan_model->count_by_status('ditolak');
         } else {
             redirect('pengajuan');
         }
