5d8a964f0738a1334c47a7dc4a114307bd753bd6
Rizky <xynnaja701@gmail.com>
2026-06-15 21:41:44 +0800

    Menambahkan konfigurasi koneksi database sistem pengajuan surat

--- application/config/database.php
+++ application/config/database.php
@@ -3,3 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $active_group = 'default';
 $query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	  => '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'sistem_pengajuan_surat',
+	'dbdriver' => 'mysqli',
+);
