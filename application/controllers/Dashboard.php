267d22c284d22fa5bf804bc14f5f4d8e135fb7c2
Rizky <xynnaja701@gmail.com>
2026-06-15 22:54:37 +0800

    Menambahkan konstruktor dan pengecekan sesi login pada Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,4 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
     
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['session']);
+        $this->load->helper(['url']);
+
+        // Cek login
+        if (!$this->session->userdata('logged_in')) {
+            redirect('login');
+        }
+    }
 }
