263d235a8e5c493970e585bd36b316301e232a17
Rizky <xynnaja701@gmail.com>
2026-06-15 22:02:33 +0800

    Menambahkan konstruktor dan load library pada Auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,4 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
     
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('User_model');
+        $this->load->library(['session', 'form_validation']);
+        $this->load->helper(['url', 'form']);
+    }
 }
