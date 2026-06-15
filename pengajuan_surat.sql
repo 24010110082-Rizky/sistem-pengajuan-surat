f8d13ef367784fe011956405fa0e58148123d658
Rizky <xynnaja701@gmail.com>
2026-06-15 21:26:14 +0800

    Menambahkan perintah untuk membuat database sistem_pengajuan_surat

--- pengajuan_surat.sql
+++ pengajuan_surat.sql
@@ -3,3 +3,9 @@
 -- Sistem Pengajuan Surat Mahasiswa
 -- Universitas Bumigora 2026
 -- ============================================
+
+CREATE DATABASE IF NOT EXISTS `sistem_pengajuan_surat`
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE `sistem_pengajuan_surat`;
