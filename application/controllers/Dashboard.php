3b0ef87238201aa593983b38b4cc6c9f62d318c9
Rizky <xynnaja701@gmail.com>
2026-06-15 22:57:42 +0800

    Menambahkan fungsi index untuk pengecekan role pengguna

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,16 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-    
+
     public function __construct()
     {
         parent::__construct();
         $this->load->library(['session']);
         $this->load->helper(['url']);
 
-        // Cek login
         if (!$this->session->userdata('logged_in')) {
             redirect('login');
+        }
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata('role_name') === 'admin') {
+            $this->load->model(['Pengajuan_model']);
+        } else {
+            redirect('pengajuan');
         }
     }
 }
