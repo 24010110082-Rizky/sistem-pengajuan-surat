1f867acedc6da4841baa652d7ddd0d943d99ecc8
Rizky <xynnaja701@gmail.com>
2026-06-15 21:42:32 +0800

    Menambahkan pengaturan tambahan koneksi database

--- application/config/database.php
+++ application/config/database.php
@@ -11,4 +11,17 @@ $db['default'] = array(
 	'password' => '',
 	'database' => 'sistem_pengajuan_surat',
 	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_unicode_ci',
+	'swap_pre' => '',
+	'encrypt'  => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
 );
