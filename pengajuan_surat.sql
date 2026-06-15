12c4013be3f7195122c8b9bc04274c9ae8aa01d9
Rizky <xynnaja701@gmail.com>
2026-06-15 21:27:36 +0800

    Menambahkan tabel roles untuk menyimpan data role pengguna

--- pengajuan_surat.sql
+++ pengajuan_surat.sql
@@ -9,3 +9,9 @@ CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 USE `sistem_pengajuan_surat`;
+
+CREATE TABLE `roles` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
