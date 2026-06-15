ae17ee023820cdb8a3936c058975bcecd9722756
Rizky <xynnaja701@gmail.com>
2026-06-15 21:54:18 +0800

    Menambahkan konstruktor dan koneksi database pada User_model

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -4,4 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User_model extends CI_Model {
 
     protected $table = 'users';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
 }
