dca85974c797cc0b1eaeec3505d4e16250f23fae
Rizky <xynnaja701@gmail.com>
2026-06-19 14:00:47 +0800

    Menambahkan konstruktor dan pengecekan sesi login pada Profil

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -3,4 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Profil extends CI_Controller {
     
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('User_model');
+        $this->load->library(['session', 'form_validation']);
+        $this->load->helper(['url', 'form']);
+
+        if (!$this->session->userdata('logged_in')) {
+            redirect('login');
+        }
+        if ($this->session->userdata('role_name') !== 'mahasiswa') {
+            redirect('dashboard');
+        }
+    }
 }
