a32ec4123a5d5c4d7e8b6ec344be2175c601a8f9
My_Github <dewadannyswara@gmail.com>
2026-06-21 21:11:42 +0700

    Menambahkan fungsi untuk mengambil detail pengajuan berdasarkan id

--- application/models/Pengajuan_model.php
+++ application/models/Pengajuan_model.php
@@ -20,4 +20,14 @@ class Pengajuan_model extends CI_Model
         $this->db->order_by('pengajuan.tanggal_ajuan', 'DESC');
         return $this->db->get()->result();
     }
+
+    public function get_by_id($id)
+    {
+        $this->db->select('pengajuan.*, jenis_surat.nama_surat, users.name as nama_mahasiswa, users.nim');
+        $this->db->from($this->table);
+        $this->db->join('jenis_surat', 'jenis_surat.id = pengajuan.jenis_surat_id');
+        $this->db->join('users', 'users.id = pengajuan.user_id');
+        $this->db->where('pengajuan.id', $id);
+        return $this->db->get()->row();
+    }
 }
