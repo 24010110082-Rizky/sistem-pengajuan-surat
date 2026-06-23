75d490be9f6743166b7b731308b48d57b10d5ec9
Ikbal <ikbalyas343@gmail.com>
2026-06-23 13:52:45 +0800

    Tambah penanda pada file Jenis_surat_model.php

--- application/models/Jenis_surat_model.php
+++ application/models/Jenis_surat_model.php
@@ -11,32 +11,38 @@ class Jenis_surat_model extends CI_Model {
         $this->load->database();
     }
 
+    // Ambil semua jenis surat aktif
     public function get_all_aktif()
     {
         return $this->db->get_where($this->table, ['status' => 'aktif'])->result();
     }
 
+    // Ambil semua jenis surat (admin)
     public function get_all()
     {
         return $this->db->get($this->table)->result();
     }
 
+    // Ambil by ID
     public function get_by_id($id)
     {
         return $this->db->get_where($this->table, ['id' => $id])->row();
     }
 
+    // Tambah jenis surat
     public function tambah($data)
     {
         return $this->db->insert($this->table, $data);
     }
 
+    // Tambah jenis surat
     public function update($id, $data)
     {
         $this->db->where('id', $id);
         return $this->db->update($this->table, $data);
     }
 
+    // Hapus jenis surat
     public function hapus($id)
     {
         return $this->db->delete($this->table, ['id' => $id]);
